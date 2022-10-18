4f24a86c3d5b08b7c180603daef60ba7ffa29aaa
yuliana-shakhvalieva <106041866+yuliana-shakhvalieva@users.noreply.github.com>
2022-10-18 23:02:20 +0300

    Change converter_to_md.py
    Added the ability to add a py_file to an existing md_file

--- converter_to_md.py
+++ converter_to_md.py
@@ -1,3 +1,6 @@
+from os.path import exists
+
+
 INPUT_CODE_DELIMITER = '# ---end----'
 
 
@@ -12,6 +15,27 @@ def write_data(file_name, data):
     file = open(file_name, 'w')
     file.write(data)
     file.close()
+
+
+def write_to_md(file_name, data):
+    if exists(file_name):
+        data = change_data(file_name, data)
+    write_data(file_name, data)
+
+
+def change_data(file_name, new_content):
+    exist_content = read_data(file_name)
+    return md_concat(exist_content, new_content)
+
+
+def md_concat(exist_content, new_content):
+    str_list = exist_content.split('\n')
+    new_link = new_content.split('\n')[0]
+
+    for index, line in enumerate(str_list):
+        if not line.startswith('+ ['):
+            str_list.insert(index, new_link)
+            return '\n'.join(str_list) + '\n' + new_content[len(new_link) + 1:]
 
 
 def prepare_md_titles(data):
@@ -52,7 +76,7 @@ def convert_data(data):
 def main():
     content = read_data('diagonal_sum.py')
     result = convert_data(content)
-    write_data('matrix.md', result)
+    write_to_md('matrix.md', result)
 
 
 if __name__ == "__main__":
