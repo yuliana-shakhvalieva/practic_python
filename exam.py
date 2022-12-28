38c8d3301875a4aa6ba4f454c3cc0595e936a5a0
yuliana-shakhvalieva <106041866+yuliana-shakhvalieva@users.noreply.github.com>
2022-12-28 11:55:13 +0300

    Update exam.py

--- exam.py
+++ exam.py
@@ -16,14 +16,14 @@ class StrategyDeal:
         return [self.bank * target / self.entry for target in self.targets]
 
     def get_str_target(self, i):
-        target = f"{i + 1} target: {self.get_targets()[i]}\n"
-        percent = f"Percent: {round(self.get_target_percents()[i], 3)}%\n"
-        bank = f"Bank: {round(self.get_target_banks()[i], 3)}"
+        target = f'{i + 1} target: {self.get_targets()[i]}\n'
+        percent = f'Percent: {round(self.get_target_percents()[i], 3)}%\n'
+        bank = f'Bank: {round(self.get_target_banks()[i], 3)}'
         return target + percent + bank
 
     def __str__(self):
         targets = '\n\n'.join([self.get_str_target(i) for i in range(len(self.targets))])
-        return f"BANK: {self.bank}\nSTART_PRICE: {self.entry}\nSTOP_PRICE: {self.close}\n\n{targets}"
+        return f'BANK: {self.bank}\nSTART_PRICE: {self.entry}\nSTOP_PRICE: {self.close}\n\n{targets}'
 
 
 def read_data(file_name):
@@ -32,10 +32,10 @@ def read_data(file_name):
 
 
 def parse_data(content):
-    deals_content = content.split("-----")
+    deals_content = content.split('-----')
     deals = []
     for deal_index, deal_raw in enumerate(deals_content):
-        if len("".join(deal_raw.split())) == 0:
+        if len(''.join(deal_raw.split())) == 0:
             continue
 
         bank_index = deal_raw.find('Bank')
@@ -57,9 +57,8 @@ def parse_data(content):
 
 
 def write_data(file_name, data):
-    f = open(file_name, 'w')
-    f.write("\n\n-----\n\n".join([str(d) for d in data]))
-    f.close()
+    with open(file_name, 'w') as f:
+        f.write('\n\n-----\n\n'.join([str(d) for d in data]))
 
 
 def main():
